21381c17a06c0acd9a51bfecacb74dfc4871bc91
uriel7580 <41644919+uriel7580@users.noreply.github.com>
2019-01-08 00:59:12 -0600

    [Add] se grega el controlador -pendiente funcionalidad de las acciones y estetica de la pagina

---
+++ tarea1/controller.js
@@ -0,0 +1,7 @@
+angular.module("ev1",[]).controller("controlador",function($scope){
+    
+    $scope.accion="yomero";
+
+    $scope.datos=[{"nombre":"Uriel","Apellidos":"Hernandez","Correo":"uriel@hotmail.com","Fecha_n":"05/07/1996","genero":"Masculino"}]
+    
+});
