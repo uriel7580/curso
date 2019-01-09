3ab8bda40598a8152c019d1b27ffce3c58431a71
uriel7580 <41644919+uriel7580@users.noreply.github.com>
2019-01-09 01:01:55 -0600

    add
    [add]libreria de bootstrap

--- tarea1/controller.js
+++ tarea1/controller.js
@@ -3,5 +3,7 @@ angular.module("ev1",[]).controller("controlador",function($scope){
     $scope.accion="yomero";
 
     $scope.datos=[{"nombre":"Uriel","Apellidos":"Hernandez","Correo":"uriel@hotmail.com","Fecha_n":"05/07/1996","genero":"Masculino"}]
-    
+    $scope.editar=function(x){
+        console.log("valor de x=",x);
+    }
 });
