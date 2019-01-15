a03b03ca244564e3ff79da3d6ec04db44606c9b9
DESKTOP-QFUP54B\Ectotec <uriel7580@gmail.com>
2019-01-15 15:11:56 -0600

    [ADD]VERSION FINAL ENTREGABLE1

--- 2/libs/controller.js
+++ 2/libs/controller.js
@@ -4,12 +4,11 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
         $scope.mensaje = "Ingresa los datos requeridos";
         $scope.nuevo = {};
 
+        //Fecha=;
         $scope.mostrar = "0";
         $scope.registros = [
-            {id:0, Nombre: "Uriel", Apellidos: "Hernandez", Correo: "uriel@hotmail.com", Fecha_n: "05/07/1996", genero: "Masculino" },
-            {id:1, Nombre: "Miguel", Apellidos: "Hernandez", Correo: "miguel@mail.com", Fecha_n: "05/07/1996", genero: "Masculino" },
-            {id:2, Nombre: "Miguel2", Apellidos: "Hernandez", Correo: "migue2l@mail.com", Fecha_n: "05/07/1996", genero: "Masculino" },
-            {id:3, Nombre: "Miguel1", Apellidos: "Hernandez", Correo: "miguel1@mail.com", Fecha_n: "05/07/1996", genero: "Masculino" }
+            { id: 0, Nombre: "Uriel", Apellidos: "Hernandez", Correo: "uriel@hotmail.com", Fecha_n: "07/05/1996", genero: "Masculino" },
+            { id: 1, Nombre: "Miguel", Apellidos: "Hernandez", Correo: "miguel@mail.com", Fecha_n: "05/07/1996", genero: "Masculino" }
         ];
 
         $scope.accion = function (x) {
@@ -21,8 +20,8 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
         $scope.guardar = function () {
             var existe = null;
             var ok = 0;
-            $scope.nuevo.id=($scope.registros[$scope.registros.length-1].id)+1;
-            
+            $scope.nuevo.id = ($scope.registros[$scope.registros.length - 1].id) + 1;
+
             $scope.mensaje = "Error en el campo: ";
             ($scope.nuevo.Correo == "" || $scope.nuevo.Correo == null) ? $scope.mensaje = $scope.mensaje + "Correo electronico " : ok++;
             ($scope.nuevo.Nombre == "" || $scope.nuevo.Nombre == null) ? $scope.mensaje = $scope.mensaje + "Nombre " : ok++;
@@ -32,30 +31,30 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
             if (ok >= 5) {
 
                 angular.forEach($scope.registros, function (value, key) {
-                     
-                    if($scope.registros[key].Apellidos == $scope.nuevo.Apellidos){
-                        if($scope.registros[key].Nombre != $scope.nuevo.Nombre){
+
+                    if ($scope.registros[key].Apellidos == $scope.nuevo.Apellidos) {
+                        if ($scope.registros[key].Nombre != $scope.nuevo.Nombre) {
                             $scope.registros[key].Correo == $scope.nuevo.Correo ?
-                            $scope.mensaje = "Error!!!, ya existe un usuario registrado con ese correo" :
-                            existe = false;
-                        }else{
+                                $scope.mensaje = "Error!!!, ya existe un usuario registrado con ese correo" :
+                                existe = false;
+                        } else {
                             $scope.registros[key].Correo == $scope.nuevo.Correo ?
-                            $scope.mensaje = "Error!!!, este usuario ya registro este correo" :
-                            existe = false;
+                                $scope.mensaje = "Error!!!, este usuario ya registro este correo" :
+                                existe = false;
                         }
-                    }else{
+                    } else {
                         $scope.registros[key].Correo == $scope.nuevo.Correo ?
                             $scope.mensaje = "Error!!!, ya existe un usuario registrado con ese correo" :
                             existe = false;
                     }
-                    
+
                 });
-                if(existe == false){
+                if (existe == false) {
                     $scope.registros.push($scope.nuevo);
                     $scope.mensaje = "Registro creado exitosamente";
-                    $scope.nuevo={};
-                } 
-                
+                    $scope.nuevo = {};
+                }
+
             }
         }
 
@@ -67,18 +66,24 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
                 }
             });
         }
-        $scope.editar = function (id,Nombre, Apellidos, genero, Fecha_n, Correo) {
-            $scope.nuevo.Correo = Correo;
-            $scope.nuevo.Nombre = Nombre;
-            $scope.nuevo.Apellidos = Apellidos;
-            $scope.nuevo.genero = genero;
-            $scope.nuevo.Fecha_n = Fecha_n;
-            $scope.nuevo.id=id;
+        $scope.editar = function (id) {
+            var ft, fechainicial;
+
+            angular.forEach($scope.registros, function (value, key) {
+                if ($scope.registros[key].id == id) {
+                    $scope.nuevo = $scope.registros[key];
+                    fT = $scope.registros[key].Fecha_n.split("/");
+                    ft = new Date(fT[2], fT[1] - 1, fT[0]);
+                    $scope.nuevo.Fecha_n = ft;
+                }
+            });
+
             $scope.mostrar = 3;
+            console.log($scope.nuevo);
         }
         $scope.actualizar = function (Nombre, Apellidos, genero, Fecha_n, Correo) {
             $scope.mensaje = "Error en el campo: ";
-            var ok=0;
+            var ok = 0;
             ($scope.nuevo.Correo == "" || $scope.nuevo.Correo == null) ? $scope.mensaje = $scope.mensaje + "Correo electronico " : ok++;
             ($scope.nuevo.Nombre == "" || $scope.nuevo.Nombre == null) ? $scope.mensaje = $scope.mensaje + "Nombre " : ok++;
             ($scope.nuevo.Apellidos == "" || $scope.nuevo.Apellidos == null) ? $scope.mensaje = $scope.mensaje + "Apellidos " : ok++;
@@ -87,26 +92,27 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
             if (ok >= 5) {
 
                 angular.forEach($scope.registros, function (value, key) {
-                    if($scope.registros[key].Correo == $scope.nuevo.Correo){
-                        if($scope.registros[key].id == $scope.nuevo.id){
-                            $scope.registros[key].Nombre=$scope.nuevo.Nombre;
-                            $scope.registros[key].Apellidos=$scope.nuevo.Apellidos;
-                            $scope.registros[key].genero=$scope.nuevo.genero;
-                            $scope.registros[key].Fecha_n=$scope.nuevo.Fecha_n;
-                            $scope.registros[key].Correo=$scope.nuevo.Correo;
+                    if ($scope.registros[key].Correo == $scope.nuevo.Correo) {
+                        if ($scope.registros[key].id == $scope.nuevo.id) {
+                            $scope.registros[key].Nombre = $scope.nuevo.Nombre;
+                            $scope.registros[key].Apellidos = $scope.nuevo.Apellidos;
+                            $scope.registros[key].genero = $scope.nuevo.genero;
+                            $scope.registros[key].Fecha_n = $scope.nuevo.Fecha_n;
+                            $scope.registros[key].Correo = $scope.nuevo.Correo;
                             $scope.mensaje = "ACTUALIZADO ";
                         }
-                    }else{
-                        if($scope.registros[key].id == $scope.nuevo.id){
-                            $scope.registros[key].Nombre=$scope.nuevo.Nombre;
-                            $scope.registros[key].Apellidos=$scope.nuevo.Apellidos;
-                            $scope.registros[key].genero=$scope.nuevo.genero;
-                            $scope.registros[key].Fecha_n=$scope.nuevo.Fecha_n;
-                            $scope.registros[key].Correo=$scope.nuevo.Correo;
+                    } else {
+                        if ($scope.registros[key].id == $scope.nuevo.id) {
+                            $scope.registros[key].Nombre = $scope.nuevo.Nombre;
+                            $scope.registros[key].Apellidos = $scope.nuevo.Apellidos;
+                            $scope.registros[key].genero = $scope.nuevo.genero;
+                            $scope.registros[key].Fecha_n = $scope.nuevo.Fecha_n;
+                            $scope.registros[key].Correo = $scope.nuevo.Correo;
                             $scope.mensaje = "ACTUALIZADO ";
                         }
                     }
                 });
+                console.log($scope.nuevo);
             }
         }
     });
