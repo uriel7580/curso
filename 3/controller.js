d4768ad3bb4f335266277b22528a5d426a0982b0
uriel7580 <41644919+uriel7580@users.noreply.github.com>
2019-01-17 23:51:40 -0600

    version final del fron

--- 3/controller.js
+++ 3/controller.js
@@ -7,8 +7,7 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
         $scope.color.mensaje = 'alert-primary';
         $scope.tema;
         $scope.mostrar = false;
-        var errores=0;
-
+        var errores = 0;
         $http({
             method: 'GET',
             url: 'http://cursos.ectotec.com/javascript/api/temas'
@@ -18,30 +17,12 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
 
         //muestra campos erroneos o faltantes
         $scope.validar = function () {
-            var i, c, letra = 0, espacio = 0;
+            errores = 0;
+            var i, c, letra = 0, espacio = 0, punto = 0;
             var factual = new Date().getFullYear();
             var fnacimiento, edad;
-            
-            $scope.color.Nombre = ($scope.datos.Nombre == "" || $scope.datos.Nombre == undefined) ? 'border-danger' : 'border-success';
-            $scope.color.Correo = ($scope.datos.Correo == "" || $scope.datos.Correo == undefined) ? 'border-danger' : 'border-success';
-            $scope.color.Fecha_n = ($scope.datos.Fecha_n == "" || $scope.datos.Fecha_n == undefined) ? 'border-danger' : 'border-success';
-            $scope.color.tema = ($scope.datos.tema == "" || $scope.datos.tema == undefined) ? 'border-danger' : 'border-success';
-            $scope.color.Comentario = ($scope.datos.Comentario == "" || $scope.datos.Comentario == undefined) ? 'border-danger' : 'border-success';
-
-
-            //validacion de edad
-            
             fnacimiento = new Date($scope.datos.Fecha_n).getFullYear();
             edad = factual - fnacimiento;
-            if (edad > 15 && edad < 120) {
-                $scope.color.Fecha_n='border-success';
-                $scope.color.Nombre = 'border-success';
-            }
-            else{
-                $scope.mensaje = "debes de ser mayor de 15 años y menor de 120 años para poder usar este servicio";
-                $scope.color.mensaje = 'alert-danger';
-            }
-            
             //validacion de nombre
             if (($scope.datos.Nombre != "" && $scope.datos.Nombre != undefined)) {
                 for (i = 0; i < $scope.datos.Nombre.length; i++) {
@@ -50,33 +31,95 @@ angular.module("appcontroller", ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
                 }
 
                 if (espacio >= 2 && letra >= espacio && letra < $scope.datos.Nombre.length) {
-                    $scope.color.mensaje = 'alert-primary';
                     $scope.mensaje = "Ingresa los datos requeridos";
                     $scope.color.Nombre = 'border-success';
+                    errores == 0 ? errores = 0 : errores++;
+                } else {
+                    errores++;
+                    $scope.color.Nombre = 'border-danger';
+                    $scope.mensaje = "El nombre debe de tener al menos 3 palabras"
                 }
+
             } else {
                 $scope.color.Nombre = 'border-danger';
-                $scope.mensaje = "El nombre debe de tener al menos 3 palabras"
-                $scope.color.mensaje = 'alert-danger';
+                errores++;
             }
-            angular.forEach($scope.registros, function (value, key) {
-                if($scope.color[key]== 'alert-danger' || $scope.color[key]=='border-danger'){errores++;}
-            });
+
+            //validacion de edad
+            if (($scope.datos.Fecha_n != "" || $scope.datos.Fecha_n != undefined) && errores == 0) {
+                if (edad > 15 && edad < 120) {
+                    $scope.color.Fecha_n = 'border-success';
+                    $scope.mensaje = "Ingresa los datos requeridos";
+                    errores == 0 ? errores = 0 : errores++;
+                }
+                else {
+                    $scope.color.Fecha_n = 'border-danger';
+                    $scope.mensaje = "debes de ser mayor de 15 años y menor de 120 años para poder usar este servicio";
+                    errores++;
+                }
+            } else {
+                $scope.color.Fecha_n = 'border-danger';
+                errores++;
+            }
+
+            //validacion de correo
+            if (($scope.datos.Correo != "" && $scope.datos.Correo != undefined)) {
+                console.log("correo", $scope.datos.Correo);
+                $scope.color.Correo = 'border-success'
+                errores == 0 ? errores = 0 : errores++;
+            } else {
+                $scope.color.Correo = 'border-danger';
+                if (errores == 0) $scope.mensaje = "Ingresa el correo correctamente mail@direccion.com";
+                errores++;
+            }
+
+            //validacion de tema
+            console.log($scope.datos.tema)
+            if (($scope.datos.tema != "" && $scope.datos.tema != undefined && $scope.datos.tema != null)) {
+                if (errores == 0) {
+                    $scope.color.tema = 'border-success';
+                    $scope.mensaje = "Ingresa los datos requeridos";
+                    errores = 0;
+                } else {
+                    $scope.color.tema = 'border-success';
+                    errores++;
+                }
+            } else {
+                $scope.color.tema = 'border-danger';
+                if (errores == 0) $scope.mensaje = "selecciona un tema";
+                errores++;
+            }
+
+            if (($scope.datos.Comentario != "" && $scope.datos.Comentario != undefined && $scope.datos.Comentario != null)) {
+                $scope.color.Comentario = 'border-success';
+                errores == 0 ? errores = 0 : errores++;
+
+            } else {
+                $scope.color.Comentario = 'border-danger';
+                if (errores == 0) $scope.mensaje = "Igresa un comentario";
+                errores++;
+            }
+            $scope.color.mensaje = errores == 0 ? 'alert-primary':'alert-danger';
         }
 
         $scope.enviar = function () {
             $scope.validar();
-            $scope.mostrar=errores==0?true:false;
+            $scope.mostrar = errores == 0 ? true : false;
         }
-        
+
         $scope.confirmar = function () {
             $http({
-            method: 'post',
-            url: "controller.php",
-            data:$scope.datos
+                method: 'post',
+                url: "controller.php",
+                data: $scope.datos
             }).then(function (response) {
                 console.log(response);
             });
         }
+        $scope.Cancelar = function () {
+            $scope.datos = {}
+            $scope.mostrar = false;
+        }
+
 
     });
