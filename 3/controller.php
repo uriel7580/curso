7d2502ad5440b510b1e900a6542ff0798dcf1401
DESKTOP-QFUP54B\Ectotec <uriel7580@gmail.com>
2019-01-21 16:19:46 -0600

    version definitiva final

--- 3/controller.php
+++ 3/controller.php
@@ -6,13 +6,15 @@ $tema=$_POST['Tema'];
 $comentario=$_POST['Comentario'];
 $fecha_hoy=date("d/m/Y H:i");
 $IP=$_SERVER['REMOTE_ADDR'];
+
 //datos del correo
 $cuerpo="De: $nombre \n";
-$cuerpo .="Correo: $correo\n";
-$cuerpo .="Fecha y hora de envio: $fecha_hoy\n";
-$cuerpo .="Fecha de nacimiento: $fecha\n";
-$cuerpo .="Comentarios: $comentario\n";
-$cuerpo .="IP: $IP";
+$cuerpo .="Correo: $correo \n";
+$cuerpo .="Fecha y hora de envio: $fecha_hoy \n";
+$cuerpo .="Fecha de nacimiento: $fecha \n";
+$cuerpo .="Comentarios: $comentario \n";
+$cuerpo .="IP: $IP \n";
+$cuerpo .="creado por uriel hernandez";
 
 if(mail($correo,$tema,$cuerpo)){
     echo '<script type="text/javascript">
@@ -21,10 +23,8 @@ if(mail($correo,$tema,$cuerpo)){
         </script>';
 }else{
     echo '<script type="text/javascript">
-       
         window.location.href="index.html";
         alert("Error,Ocurrio un error durante el envio");
         </script>';
 }
-
 ?>
